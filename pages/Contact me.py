f1f6853a1c625bbafc37c84c0c186351941a28ed
GunZ_Lap2 <gunzeux71@gmail.com>
2024-09-19 12:44:22 +0530

    Organised the contact me form present

--- pages/Contact me.py
+++ pages/Contact me.py
@@ -1,3 +1,17 @@
 import streamlit as st
+from send_email import send_email
 
 st.title("Contact Me")
+
+with st.form("send message"):
+    user_mail = st.text_input("Email")
+    raw_text = st.text_area("Enter your Message")
+    send = st.form_submit_button("Submit")
+
+    if send:
+        message = f"""Subject: new message from {user_mail} at web portfolio
+        
+user email : {user_mail}
+message : {raw_text}"""
+        send_email(message)
+        st.info("email sent successfully".title())
